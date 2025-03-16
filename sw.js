0fb8d7620695a11df1cc2520793fca600ad0e958
kchikuse <kchikuse@users.noreply.github.com>
2025-03-16 15:49:00 +0000

    deploy: 56b5e9606e1b46036044dcff8febb0eb05eae176

--- sw.js
+++ sw.js
@@ -9,22 +9,32 @@ self.addEventListener("activate", function (event) {
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
 
-  const quoteIndex = event.notification.data
-    ? event.notification.data.quoteIndex
-    : undefined;
+  const message = {
+    type: "NOTIFICATION_CLICK",
+    quoteIndex: event.notification.data
+      ? event.notification.data.quoteIndex
+      : undefined,
+  };
 
   event.waitUntil(
     clients.matchAll({ type: "window" }).then(function (clientList) {
       if (clientList.length > 0) {
         clientList[0].focus();
-        clientList[0].postMessage({
-          type: "NOTIFICATION_CLICK",
-          quoteIndex: quoteIndex,
-        });
+        clientList[0].postMessage(message);
         return;
       }
 
-      return clients.openWindow("/");
+      // If no client is open, open window
+      return clients.openWindow("/").then(() => {
+        // Wait a bit for the page to load before sending message
+        setTimeout(() => {
+          self.clients.matchAll({ type: "window" }).then(function (windows) {
+            if (windows.length > 0) {
+              windows[0].postMessage(message);
+            }
+          });
+        }, 1000);
+      });
     })
   );
 });
