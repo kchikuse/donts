f64c9e6e08cae0997903a2d4fd4abecd0aa6f2ba
KC <kchikuse@users.noreply.github.com>
2025-03-16 13:31:27 +0200

    Delete src directory

--- src/app/app.guard.ts
+++
@@ -1,9 +0,0 @@
-import { ActivatedRouteSnapshot, CanActivateFn, Route } from '@angular/router';
-import { Role } from './app.config';
-
-export function navGuard(): CanActivateFn {
-  return (route: ActivatedRouteSnapshot) => {
-    const role = route.paramMap.get('role') as Role;
-    return ['husbands', 'wives'].includes(role);
-  };
-}
